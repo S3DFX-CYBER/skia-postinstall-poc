124dafc2c4e8d60c7bc04fefe6cb5fec688986d2
Savio Dsouza <saviodsouza8a@gmail.com>
2026-06-12 14:26:34 +0530

    Update install-libs.js

--- fake-package/scripts/install-libs.js
+++ fake-package/scripts/install-libs.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
 const os = require('os');
-const { execSync } = require('child_process');
 
-console.log('=== POSTINSTALL EXECUTED ===');
+console.log('================================');
+console.log('POSTINSTALL EXECUTED');
+console.log('================================');
 console.log('Hostname:', os.hostname());
 console.log('User:', os.userInfo().username);
 console.log('Platform:', os.platform());
 
-// In a real attack this would be:
-// process.env.GITHUB_TOKEN exfiltrated here
-// We just log to prove execution
-console.log('GITHUB_TOKEN present:', !!process.env.GITHUB_TOKEN);
-console.log('Token prefix:', process.env.GITHUB_TOKEN?.slice(0, 8));
+fs.writeFileSync(
+  '/tmp/postinstall-proof.txt',
+  JSON.stringify({
+    executed: true,
+    hostname: os.hostname(),
+    user: os.userInfo().username,
+    platform: os.platform(),
+    timestamp: new Date().toISOString()
+  }, null, 2)
+);
+
+console.log('Proof file written');
